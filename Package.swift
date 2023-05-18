7e98c89af2bfc9b4a6a16f7846abdb17bccd1e96
zhengruyan <zhengruyan@bytedance.com>
2023-05-18 19:25:18 +0800

    update version to 5206

--- Package.swift
+++ Package.swift
@@ -5,22 +5,61 @@ import PackageDescription
 
 let package = Package(
     name: "AdsGlobalPackage",
+    defaultLocalization: "en",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AdsGlobalPackage",
             targets: ["AdsGlobalPackage"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(name: "PAGAdSDK",
+                      url:"https://sf16-fe-tos-sg.i18n-pglstatp.com/obj/pangle-sdk-static-va/5.2.0.6/PAGAdSDK.xcframework.zip",
+                      checksum: "b86b7f65e6a15e562191ff905aace12e21f27f6f1baaa81f281f119a33adfe0b"),
+        .binaryTarget(name: "BURelyAdSDK",
+                      url:"https://sf16-fe-tos-sg.i18n-pglstatp.com/obj/pangle-sdk-static-va/BURelyAdSDK/2.2.0.2/BURelyAdSDK.xcframework.zip",
+                      checksum: "e009f77667dbf2bdbe5d57fa206c77e634d1e7ca370f60d56398e907a7f7701f"),
+        .binaryTarget(name: "BURelyFoundation_Global",
+                      url:"https://sf16-fe-tos-sg.i18n-pglstatp.com/obj/bu-rely-framework-sg/0.1.0.16/framework/BURelyFoundation_Global.zip",
+                      checksum: "deaa573af37d581d53960b75f9f23d13ef8f052195ef0874d7a61cfd645d12e6"),
         .target(
             name: "AdsGlobalPackage",
-            dependencies: []),
+            dependencies: [
+                .target(name: "PAGAdSDK"),
+                .target(name: "BURelyAdSDK"),
+                .target(name: "BURelyFoundation_Global")
+            ],
+            resources: [.copy("PAGAdSDK.bundle")],
+            linkerSettings: [
+                .linkedFramework("UIKit"),
+                .linkedFramework("WebKit"),
+                .linkedFramework("MediaPlayer"),
+                .linkedFramework("AdSupport"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("CoreTelephony"),
+                .linkedFramework("StoreKit"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("MobileCoreServices"),
+                .linkedFramework("CoreMotion"),
+                .linkedFramework("Accelerate" ),
+                .linkedFramework("AudioToolbox"),
+                .linkedFramework("JavaScriptCore"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("Security"),
+                .linkedFramework("DeviceCheck"),
+                .linkedFramework("CoreML"),
+                .linkedFramework("AppTrackingTransparency"),
+                .linkedLibrary("z"),
+                .linkedLibrary("resolv"),
+                .linkedLibrary("sqlite3"),
+                .linkedLibrary("bz2"),
+                .linkedLibrary("xml2"),
+                .linkedLibrary("iconv"),
+                .linkedLibrary("c++abi"),
+                .linkedLibrary("c++"),
+            ]),
         .testTarget(
             name: "AdsGlobalPackageTests",
             dependencies: ["AdsGlobalPackage"]),
